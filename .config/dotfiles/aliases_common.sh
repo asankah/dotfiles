e7d276ef23fb4a29b74a66f959ba815c602e859b
Asanka Herath <asanka@google.com>
2022-09-13 15:08:04 -0400

    aliases_common

---
+++ .config/dotfiles/aliases_common.sh
@@ -0,0 +1,18 @@
+alias cd='cd -P'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias l='ls -CF'
+alias la='ls -A'
+
+alias gca='git amend'
+alias gp='git push'
+alias gsw='git switch'
+
+alias icat="kitty +kitten icat"
+alias gud="vcs-upstream-diff"
+alias gup="vcs-upstream-fetch"
+alias st="vcs-status"
+alias gm="vcs-xl"
+
+
